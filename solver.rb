0bcdc0206b6fcc6e271bd6cf807327bf4f0871e1
Munezero Christian <chamsf5@aol.com>
2022-06-24 17:43:15 +0200

    Add factorial method

---
+++ solver.rb
@@ -0,0 +1,11 @@
+class Solver
+  def factorial(num)
+    if num.zero?
+      1
+    elsif num.positive?
+      num * factorial(num - 1)
+    else
+      'Please use a positive number'
+    end
+  end
+end
